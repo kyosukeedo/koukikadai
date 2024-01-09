26ddde6bf84a8a03c0d2315fc43919ad08b12002
kyosukeedo <131092531+kyosukeedo@users.noreply.github.com>
2024-01-10 05:16:15 +0900

    Update ex1-2.js

--- ex1-2.js
+++ ex1-2.js
@@ -1,8 +1,24 @@
-const roulette = document.querySelector('#roulette ul');
-const startButton = document.querySelector('#start');
+const startButton = document.getElementById('start-button');
+const rouletteItems = document.getElementById('roulette-items');
+let isSpinning = false;
+let spinDuration;
 
 startButton.addEventListener('click', () => {
-  const itemCount = roulette.children.length;
+  if(isSpinning) return;
 
-  const randomIndex = Math.floor(Math.random() * itemCount);
+  isSpinning = true;
+  spinDuration = Math.random() * 3000 + 3000;
+
+  rouletteItems.style.transition = 'top ${spinDuration}ms cubic-bezier(.1, .1, .1, 1)';
+  rouletteItems.style.top = '-${rouletteItems.offsetHeight}px';
+
+  setTimeout(() => {
+    isSpinning = false;
+    rouletteItems.style.transition = '';
+    rouletteItems.style.top ='0px';
+
+    const selectedItemIndex = Math.floor(Math.random() * rouletteItems.children.length);
+    const selectedItem = rouletteItems.children[selectedItemIndex];
+    alert('今日の部位は${selectedItem.textContent}です！');
+  },spinDuration);
 });
